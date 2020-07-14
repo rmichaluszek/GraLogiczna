739523974aedea8557c3cb98ad4c6b03db1f1e6b
rmichaluszek <rmichaluszek@gmail.com>
2020-07-14 22:11:47 +0200

    Added game starting and drawing the scene

--- GraWisielec/Program.cs
+++ GraWisielec/Program.cs
@@ -11,9 +11,47 @@ namespace GraWisielec
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Witaj w grze wisielec, naciśnij dowolny przycisk aby rozpocząć.");
 
             Console.ReadKey();
+
+            game();
+        }
+
+        static void game()
+        {
+            int triesLeft = 11;
+            String word = "domek";
+
+            while(triesLeft>0)
+            {
+                draw(triesLeft, word, "S");
+
+                Console.ReadLine();
+                triesLeft--;
+            }
+           
+        }
+
+        static void draw(int triesLeft, String word, String guessedLetters)
+        {
+            Console.Clear();
+            Console.Write("Próby: ");
+            for(int i =0; i< 11; i++)
+            {
+                if(i+1 > 11-triesLeft)
+                {
+                    Console.Write("[ ]");
+                } else
+                {
+                    Console.Write("[X]");
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Słowo: ");
+
+
+            Console.WriteLine();
         }
     }
 }
