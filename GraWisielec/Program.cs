7c1ec11a68bfbd34f1b229b441a9a4d4ec29af55
rmichaluszek <rmichaluszek@gmail.com>
2020-07-14 22:41:53 +0200

    Added game winning and losing

--- GraWisielec/Program.cs
+++ GraWisielec/Program.cs
@@ -21,15 +21,51 @@ namespace GraWisielec
         static void game()
         {
             int triesLeft = 11;
+            int lettersToGuessLeft = 5;
             String word = "domek";
+            String guessedLetters = "";
 
             while(triesLeft>0)
             {
-                draw(triesLeft, word, "S");
+                draw(triesLeft, word, guessedLetters);
 
-                Console.ReadLine();
+                char letter = Console.ReadKey().KeyChar;
+                if(Char.IsLetter(letter))
+                {
+                    if(!guessedLetters.Contains(letter))
+                    {
+                        guessedLetters += letter;
+                        if(word.Contains(letter))
+                        {
+                            lettersToGuessLeft--;
+                        }
+                    }
+                    
+                    if(lettersToGuessLeft<=0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Gratulacje! Słowo to: " + word);
+                        Console.WriteLine("Nacisnij dowolny przycisk aby zagrać ponownie.");
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+                else
+                {
+                    triesLeft++;
+                    //nie podano litery wiec nie zabiera tury
+                }
                 triesLeft--;
+                if(triesLeft<=0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Przykro mi, nie udało ci się. Szukane słowo to: " + word);
+                    Console.WriteLine("Nacisnij dowolny przycisk aby zagrać ponownie.");
+                    Console.ReadKey();
+                }
             }
+
+            game();
            
         }
 
