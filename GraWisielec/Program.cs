e7ab5a04045a8b146643267dca548db2d13588da
rmichaluszek <rmichaluszek@gmail.com>
2020-07-14 21:53:21 +0200

    Project cleanup, switch to console

---
+++ GraWisielec/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraWisielec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world!");
+
+            Console.ReadKey();
+        }
+    }
+}
